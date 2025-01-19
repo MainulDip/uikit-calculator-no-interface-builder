301b039f51bb0cab905d7e140fd131d00ad0597c
Mainul Dip <mainuldip@Mainuls-MacBook-Pro.local>
2025-01-18 20:30:05 -0500

    ios uikit calculator with programmatic ui initial

---
+++ uikit-calculator-no-interface-builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-calculator-no-interface-builder
+//
+//  Created by Mainul Dip on 1/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
