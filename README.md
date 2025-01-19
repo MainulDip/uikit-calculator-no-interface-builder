c17990f881452b139a2c5ece14bd021f31910380
Mainul Dip <mainuldip@Mainuls-MacBook-Pro.local>
2025-01-18 21:29:16 -0500

    ios calculator target screenshot assets docking

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ### Overviews:
+
+### Note:
+Git create a new branch and switch to current `git checkout -b my-branch` | `git branch -l` to list all branches
--- uikit-calculator-no-interface-builder/ViewController.swift
+++ uikit-calculator-no-interface-builder/ViewController.swift
@@ -12,6 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        let containerView = UIView()
+        view.addSubview(containerView)
+        view.backgroundColor = .yellow
+        containerView.backgroundColor = .blue
+        // containerView.frame = CGRect(x: 0, y: 0, width: 300, height: 400)
+        
+        // auto layout impl
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
     }
 
 
