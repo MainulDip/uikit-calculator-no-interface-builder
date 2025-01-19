5ad942decda29c01e7be612d699a09304a8e75b9
Mainul Dip <mainuldip@Mainuls-MacBook-Pro.local>
2025-01-19 15:32:58 -0500

    controls UIStackView init

--- uikit-calculator-no-interface-builder/ViewController.swift
+++ uikit-calculator-no-interface-builder/ViewController.swift
@@ -26,14 +26,48 @@ class ViewController: UIViewController {
         let isLandscape = self.view.frame.width > self.view.frame.height
         self.equationViewHeightConstraint = view.heightAnchor.constraint(equalToConstant:  self.view.frame.height * 0.15)
         equationViewHeightConstraint.isActive = true
+        
+        // inner UITextView setup
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .purple
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.textAlignment = .right
+        textView.textColor = .white
+        textView.text = "123,456+301"
+        textView.font = .systemFont(ofSize: 40, weight: .bold)
+        
+        view.addSubview(textView)
+        
+        textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        textView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         return view
     }()
     
     private lazy var preEquationView: UIView = {
         let view = UIView()
         view.backgroundColor = .lightGray
-        self.preEquationViewHeightConstraint =  view.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.1)
+        self.preEquationViewHeightConstraint =  view.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.09)
         self.preEquationViewHeightConstraint.isActive = true
+        
+        // inner UITextView setup
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .purple
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.textAlignment = .right
+        textView.textColor = .lightGray
+        textView.text = "123,757"
+        textView.font = .systemFont(ofSize: 24, weight: .bold)
+        
+        view.addSubview(textView)
+        
+        textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        textView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
         return view
     }()
@@ -41,14 +75,28 @@ class ViewController: UIViewController {
     private lazy var controlsView: UIView = {
         let view = UIView()
         view.backgroundColor = .brown
-        self.controlsViewHeightConstraint = view.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.1)
+        self.controlsViewHeightConstraint = view.heightAnchor.constraint(equalToConstant: self.view.frame.height * 0.09)
         self.controlsViewHeightConstraint.isActive = true
+        
+        // add horizontal UIStackView as container
+        // place 2 horizontal UIStackView as containers subView
+        // add 3 button containing icons to the left
+        // add 1 button containing icon to the right
+        
+        let horizontalStackViewContainer = UIStackView()
+        horizontalStackViewContainer.axis = .horizontal
+        
         return view
     }()
     
     private lazy var buttonHolderView: UIView = {
         let view = UIView()
         view.backgroundColor = .cyan
+        
+        // add a vertical UIStackView as container
+        // place 5 horizontal UIStackView as containers subView
+        // each horizontal one will hold 4 buttons with icons with proper spacing and styling
+        
         return view
     }()
     
