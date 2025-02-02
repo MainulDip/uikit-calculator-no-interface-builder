738885de09a1e1f90ec962387b7a6928fd11b386
MainulDip <mainuldip@gmail.com>
2025-02-02 18:22:26 -0500

    new instruction

---
+++ uikit-calculator-no-interface-builder/Models/CalcBtnUIModel.swift
@@ -0,0 +1,15 @@
+//
+//  CalcBtnModel.swift
+//  uikit-calculator-no-interface-builder
+//
+//  Created by Mainul Dip on 2/1/25.
+//
+
+
+import UIKit
+
+struct CalcBtnUIModel {
+    let title: String
+    let titleColor: UIColor
+    let backgroundColor: UIColor
+}
